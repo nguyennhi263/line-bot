0f3090c9a9d45f8fc722998b0a0dd835a3afcd99
Nguyen Nhi <nguyennhi263@gmail.com>
2018-11-21 11:29:30 +0700

    test message

--- index.php
+++ index.php
@@ -4,23 +4,21 @@ define("LINE_MESSAGING_API_CHANNEL_TOKEN",
     'oPJj2g+gPNOvLAtQz0CWyY6dkj/lIw86ZB2MtqPoWp5jN0EZDF4tXiIdR32FbpGYGQPvI6Wtxg0IwBPEyzUMyeVJdhhCyZo1cP1z+HQ/UV+9fHbbsUrueMZqrzMYkMNNuxoDTJXKWzM1PY6LurByvwdB04t89/1O/w1cDnyilFU=');
 
 require __DIR__."/vendor/autoload.php";
-
-$bot = new \LINE\LINEBot(
-    new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN),
-    ['channelSecret' => LINE_MESSAGING_API_CHANNEL_SECRET]
-);
-
-$signature = $_SERVER["HTTP_".\LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
-$body = file_get_contents("php://input");
-
-$events = $bot->parseEventRequest($body, $signature);
-
-foreach ($events as $event) {
-    if ($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage) {
-        $reply_token = $event->getReplyToken();
-        $text = $event->getText();
-        $bot->replyText($reply_token, $text);
+require_once (__DIR__.'/vendor /linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+$client = new LINEBotTiny('oPJj2g+gPNOvLAtQz0CWyY6dkj/lIw86ZB2MtqPoWp5jN0EZDF4tXiIdR32FbpGYGQPvI6Wtxg0IwBPEyzUMyeVJdhhCyZo1cP1z+HQ/UV+9fHbbsUrueMZqrzMYkMNNuxoDTJXKWzM1PY6LurByvwdB04t89/1O/w1cDnyilFU=',
+    'dcd4d682871e3013f2fe9f0cd14a3b0c');
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $request = $message['text'];
+                    /** get key words from request
+                     * then remove kanji , etc */
+                    //$model_nm = Logic::validateTextSearch($request);
+                    $model_nm = $request;
+                    $client->replyMessage($request);
+            }
     }
 }
-
-echo "OK";
