4a5c220a916d251c7a28be60eca6d9f838ed632a
Nguyen Nhi <nguyennhi263@gmail.com>
2018-11-21 10:21:31 +0700

    test message

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ define("LINE_MESSAGING_API_CHANNEL_SECRET", 'dcd4d682871e3013f2fe9f0cd14a3b0c');
 define("LINE_MESSAGING_API_CHANNEL_TOKEN",
     'oPJj2g+gPNOvLAtQz0CWyY6dkj/lIw86ZB2MtqPoWp5jN0EZDF4tXiIdR32FbpGYGQPvI6Wtxg0IwBPEyzUMyeVJdhhCyZo1cP1z+HQ/UV+9fHbbsUrueMZqrzMYkMNNuxoDTJXKWzM1PY6LurByvwdB04t89/1O/w1cDnyilFU=');
 
-require __DIR__."/../vendor/autoload.php";
+require __DIR__."/vendor/autoload.php";
 
 $bot = new \LINE\LINEBot(
     new \LINE\LINEBot\HTTPClient\CurlHTTPClient(LINE_MESSAGING_API_CHANNEL_TOKEN),
