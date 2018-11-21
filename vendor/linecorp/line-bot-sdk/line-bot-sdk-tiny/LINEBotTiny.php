8b7d5ef12bb5c7054a102f86b7ca4a623838c1a0
Nguyen Nhi <nguyennhi263@gmail.com>
2018-11-21 13:48:11 +0700

    test message

--- vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php
+++ vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php
@@ -110,7 +110,7 @@ class LINEBotTiny
         curl_setopt($curl_handle, CURLOPT_URL,'https://api.line.me/v2/bot/message/reply');
         curl_setopt($curl_handle, CURLOPT_CONNECTTIMEOUT, 2);
         curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl_handle, CURLOPT_USERAGENT, 'Your application name');
+        curl_setopt($curl_handle, CURLOPT_USERAGENT, $context);
         $response = curl_exec($curl_handle);
         curl_close($curl_handle);
 
